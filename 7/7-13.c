c6ea8a6b2fdc65cea01a81554d1d0fe6e3e68e15
YeongChan23 <127271715+YeongChan23@users.noreply.github.com>
2023-04-11 15:37:54 +0900

    7

---
+++ 7/7-13.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main()
+{
+	int hap;
+	int a, b, c;
+
+	printf ("합계의 시작값 ==> ");
+	scanf("%d", &a);
+
+	printf("합계의 끝값 ==> ");
+	scanf("%d", &b);
+
+	printf("배수 ==> ");
+	scanf("%d", &c);
+	
+
+
+
+
+
+	printf("%d부터 %d까지의 %d배수의 합계 ==> %d \n", a, b, c, hap);
+}
+
