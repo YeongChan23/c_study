a659c829c68f896030a4b5502368ab4f60e0750e
YeongChan23 <127271715+YeongChan23@users.noreply.github.com>
2023-04-13 21:56:56 +0900

    Add files via upload

---
+++ 3/3-3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("%d / %d =%d", 100 , 200, 0.5);//3개씩 대응하지만 %d는 정수형이라 .5는 버림 
+	printf("\n");
+}
---
+++ 3/3-4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("%d / %d = %f", 100, 200, 0.5);//%d는 정수형 %f는 실수형
+	printf("\n");
+
+	printf("%c %c", 'a', 'k');//%c는 문자형 작은따옴표
+	printf("\n");
+
+	printf("%s %s","안녕", "C 언어");//%s는 문자열형 큰따옴표
+	printf("\n");
+}
+
